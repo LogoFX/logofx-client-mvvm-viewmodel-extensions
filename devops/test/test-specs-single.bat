e91ae8241dc34ac94adbaabb7b93b5a2111a35d8
Gennady Verdel <godrose@gmail.com>
2021-06-23 21:00:55 +0300

    chore: fixed living doc output

--- devops/test/test-specs-single.bat
+++ devops/test/test-specs-single.bat
@@ -5,4 +5,4 @@ dotnet test %1.csproj -c Release
 rem TODO: Provide more generic and reusable way
 cd ./Bin/Release
 livingdoc test-assembly %1.dll -t TestExecution.json --work-item-url-template https://github.com/LogoFX/logofx-client-mvvm-viewmodel-extensions/issues/{id} --work-item-prefix WI
-cd ../../../devops/test
+cd ../../../../devops/test
